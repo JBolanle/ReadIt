b75e2d261aa8b5187fa2e0eec18e5487f7698ac6
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-12-12 14:42:36 -0600

    added view displaying book information in a search

---
+++ BookDetailCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailCell.swift
+//  ReadIt
+//
+//  Created by Jumoke Bolanle on 12/12/23.
+//
+
+import SwiftUI
+
+struct BookDetailCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookDetailCell()
+}
