40b26a3f47feb0976ee1fbf1156348b418a1b388
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-12-12 11:32:15 -0600

    added menu when add new book is pressed. minor changes to book detail view.

---
+++ LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  ReadIt
+//
+//  Created by Jumoke Bolanle on 12/12/23.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ ReadIt/View/AddNewBookManuallyView.swift
@@ -0,0 +1,23 @@
+//
+//  AddNewBookView.swift
+//  ReadIt
+//
+//  Created by Jumoke Bolanle on 12/12/23.
+//
+
+import SwiftUI
+
+struct AddNewBookManuallyView: View {
+    @State private var searchText = ""
+    @State private var searchPrompt = "Search by title, author, or ISBN."
+    
+    var body: some View {
+        VStack {
+            Text("hi")
+        }
+    }
+}
+
+#Preview {
+    AddNewBookManuallyView()
+}
